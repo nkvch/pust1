66b2ef7ad031dfd901ddf3c39198194460580b83
Stanislau Stankevich <nkvch.st@gmail.com>
2021-11-30 09:32:54 +0100

    projetk 3 start

--- BLOCK3/matlab/zad2.m
+++ BLOCK3/matlab/zad2.m
@@ -5,41 +5,56 @@ Upp = 0;
 Ypp = 0;
 n = 300; %d³ugosc symulacji
 
-inputs = zeros(1, 6);
+inputs = zeros(1, 7);
 inputs(1) = 0;
-responses = zeros(1, 6);
+responses = zeros(1, 7);
 responses(1) = 0;
 
 U(1:n) = 0;
 Y(1:n) = 0;
 
-marks(1:5) = string();
+marks(1:7) = string();
+figure;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %tor wejœcie-wyjœcie skok sygnalu w chwili k=10
-for i=1:5
-    inputs(i + 1) = i;
+nrskoku = 1;
+for i=-0.5:0.25:1
+    inputs(nrskoku) = i
     U(10:n) = i;
     for k = 10:n
         Y(k) = symulacja_obiektu10y_p3(U(k-5),U(k-6),Y(k-1),Y(k-2)); %symulacja obiektu;
     end
     stairs(Y);
-    marks(i) = sprintf('Skok U z %.2f do %.2f', 0, i);
-    responses(i + 1) = Y(n);
+    marks(nrskoku) = sprintf('Skok U z %.2f do %.2f', 0, i);
+    responses(nrskoku) = Y(n);
     hold on;
+    nrskoku = nrskoku + 1;
 end
 
 xlabel('k');
 ylabel('Y(k)');
 legend(marks,'Location', 'southeast');
-set(gcf,'Units','centimeters','Position', [ 1 1 15 10]);
-matlab2tikz('../sprawozdanie/rysunki/skokowe_wejscie_wyjscie.tex');
+set(gcf,'Units','centimeters','Position', [ 1 1 20 20]);
+matlab2tikz('../sprawozdanie/rysunki/skokowe_zad2.tex');
 
-%charakterystyka statyczna
-figure;
-plot(inputs, responses);
-set(gcf,'Units','centimeters','Position', [ 1 1 15 15]);
-matlab2tikz('../sprawozdanie/rysunki/wykres_char_stat_uy.tex');
+% 
+% %charakterystyka statyczna
+% figure;
+% plot(inputs, responses);
+% set(gcf,'Units','centimeters','Position', [ 1 1 15 15]);
+% matlab2tikz('../sprawozdanie/rysunki/wykres_char_stat_uy.tex');
+% 
+% K_stat1=(responses(6) - responses(1))/(inputs(6) - inputs(1))
 
-K_stat1=(responses(6) - responses(1))/(inputs(6) - inputs(1))
-
+% xlabel('k');
+% ylabel('Y(k)');
+% ylim([0 10]);
+% legend(marks,'Location', 'southeast');
+% set(gcf,'Units','centimeters','Position', [ 1 1 15 10]);
+% matlab2tikz('../sprawozdanie/rysunki/skokowe_zaklocenie_wyjscie.tex');
+% 
+% %charakterystyka statyczna
+% figure;
+% plot(inputs, responses);
+% set(gcf,'Units','centimeters','Position', [ 1 1 15 15]);
 
